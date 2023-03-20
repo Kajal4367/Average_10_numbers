6b6e85df5011fdd814869cf4f240fefec80c445b
Kajal Kumari <96781807+Kajal4367@users.noreply.github.com>
2023-03-20 14:51:49 +0530

    Create Average_10_numbers.py

---
+++ Average_10_numbers.py
@@ -0,0 +1,9 @@
+# Average of 10 numbers using while loop
+i = 0
+sum = 0
+while(i!=10):
+  a = int(input("Enter a number:"))
+  sum = sum + a
+  i = i + 1
+  avg = sum/10
+print("Average of 10 numbers =",avg)
